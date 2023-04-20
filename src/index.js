3850123c71456cabcb23c6c2a05d5516647fbf2e
Miguel Ferre <miguelferreporcel@gmail.com>
2023-04-20 20:38:13 +0200

    Form Posts styles

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { legacy_createStore as createStore , applyMiddleware, compose } from 'redux';
-import thunk from 'redux-thunk';
+import ReactDOM from 'react-dom'
+import App from './App';
+
+ReactDOM.render(<App />, document.getElementById('root'),
+);
