c65d7ae489a6365e5f56131aeca6f2bf408cd451
Miguel Ferre <miguelferreporcel@gmail.com>
2023-05-19 10:44:54 +0200

    memories

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
+import { useDispatch } from 'react-redux';
+import { getPosts } from './actions/posts'
 import logo from './images/logo.gif'
 import Posts from './components/Posts/Posts'
 import Form from './components/Form/Form'
@@ -7,17 +9,27 @@ import useStyles from './styles'
 
 const App = () => {
   const classes = useStyles()
+  const dispatch = useDispatch()
+
+  useEffect(() => {    dispatch(getPosts());
+  }, [dispatch]);
 
   return (
     <Container>
-      <AppBar className={classes.appBar} position='static' color='inherit'>
-        <img className={classes.image} src={logo} alt='logo' height = '60px' />
-        <Typography className={classes.heading} variant='h2' align='center'>alikindoiMemories</Typography>
-        
+      <AppBar className={classes.appBar} position="static" color="inherit">
+        <img className={classes.image} src={logo} alt="logo" height="60px" />
+        <Typography className={classes.heading} variant="h2" align="center">
+          alikindoiMemories
+        </Typography>
       </AppBar>
       <Grow in>
         <Container>
-          <Grid container justify="space-between" alignItems="stretch" spacing={3}>
+          <Grid
+            container
+            justify="space-between"
+            alignItems="stretch"
+            spacing={3}
+          >
             <Grid item xs={12} sm={7}>
               <Posts />
             </Grid>
@@ -28,9 +40,7 @@ const App = () => {
         </Container>
       </Grow>
     </Container>
-
-    
-  )
+  );
 }
 
 export default App
